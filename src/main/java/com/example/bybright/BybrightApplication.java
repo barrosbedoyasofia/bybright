05aab0dbd913f88e8a122f0246785f16b7319496
sofia barros <barrosbedoyasofia@gmail.com>
2023-02-28 20:38:25 -0500

    Se crea conexion a base de datos

---
+++ src/main/java/com/example/bybright/BybrightApplication.java
@@ -0,0 +1,13 @@
+package com.example.bybright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BybrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BybrightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bybright/BybrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bybright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BybrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
